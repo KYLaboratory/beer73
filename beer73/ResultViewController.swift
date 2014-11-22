b142c593d79f292252f250701aca95914672573d
住吉信一 <sumiyoshishinichi@sumiyoshishinichi-no-Mac-mini.local>
2014-11-22 17:49:55 +0900

    back home button add twitter add

--- beer73/ResultViewController.swift
+++ beer73/ResultViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Social
 
 class ResultViewController : UIViewController {
     
@@ -17,7 +18,11 @@ class ResultViewController : UIViewController {
     var pernum : Float32 = 0.0;
     
     var scoreShowFlag : Int32 = 1
+    var countUp : Int32 = 0
     
+    var myButtonBefore: UIButton!
+    var myComposeView : SLComposeViewController!
+    var myTwitterButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -136,12 +141,18 @@ class ResultViewController : UIViewController {
             pernum += Float32(score) * 0.01;
             myProgressView.setProgress(pernum, animated: true)
         }else{
-            self.showScore()
+            self.showScoreViews()
             scoreShowFlag *= -1
+        }
+        
+        // Buttonポップアップ
+        countUp++
+        if countUp > 7 {
+            self.showButtons()
         }
     }
 
-    func showScore(){
+    func showScoreViews(){
         self.showScoreLabel()
         self.showScoreBackView()
     }
@@ -204,7 +215,70 @@ class ResultViewController : UIViewController {
         
         self.view.addSubview(myImageView)
     }
-
+    
+    func showButtons(){
+        // UIボタンを作成
+        myButtonBefore = UIButton(frame: CGRectMake(0,0,120,50))
+        myButtonBefore.backgroundColor = UIColor.redColor();
+        myButtonBefore.layer.masksToBounds = true
+        myButtonBefore.setTitle("撮影画面へ", forState: .Normal)
+        myButtonBefore.layer.cornerRadius = 20.0
+        myButtonBefore.layer.position = CGPoint(x: self.view.bounds.width/2-100 , y:self.view.bounds.height-50)
+        myButtonBefore.addTarget(self, action: "onClickMyButton:", forControlEvents: .TouchUpInside)
+        self.view.addSubview(myButtonBefore);
+        
+        // Twitter用のUIButtonの生成.
+        let hex: Int = 0x55ACEE
+        let red = Double((hex & 0xFF0000) >> 16) / 255.0
+        let green = Double((hex & 0xFF00) >> 8) / 255.0
+        let blue = Double((hex & 0xFF)) / 255.0
+        var myColor: UIColor = UIColor( red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: CGFloat(1.0))
+        
+        // ボタン.
+        myTwitterButton = UIButton()
+        myTwitterButton.frame = CGRectMake(0,0,100,100)
+        myTwitterButton.backgroundColor = myColor
+        myTwitterButton.layer.masksToBounds = true
+        myTwitterButton.setTitle("Twitter", forState: UIControlState.Normal)
+        myTwitterButton.titleLabel?.font = UIFont.systemFontOfSize(CGFloat(20))
+        myTwitterButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        myTwitterButton.layer.cornerRadius = 50.0
+        myTwitterButton.layer.position = CGPoint(x: self.view.frame.width/2+100, y:self.view.bounds.height-50)
+        myTwitterButton.tag = 1
+        myTwitterButton.addTarget(self, action: "onPostToTwitter:", forControlEvents: .TouchUpInside)
+        
+        // buttonをviewに追加.
+        self.view.addSubview(myTwitterButton)
+    }
+    
+    func onClickMyButton(sender: UIButton){
+        
+        // 遷移するViewを定義.
+        let camViewController: UIViewController = CameraViewController()
+        
+        // アニメーションを設定.
+        camViewController.modalTransitionStyle = UIModalTransitionStyle.FlipHorizontal
+        
+        // Viewの移動.
+        self.presentViewController(camViewController, animated: true, completion: nil)
+    }
+    
+    func onPostToTwitter(sender : AnyObject) {
+        // SLComposeViewControllerのインスタンス化.
+        // ServiceTypeをTwitterに指定.
+        myComposeView = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        
+        // 投稿するテキストを指定.
+        myComposeView.setInitialText("Twitter Test from Swift")
+        
+        // 投稿する画像を指定.
+        myComposeView.addImage(UIImage(named: "beer.JPG"))
+        
+        // myComposeViewの画面遷移.
+        self.presentViewController(myComposeView, animated: true, completion: nil)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
