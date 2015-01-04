be3a9755d4000fe66457989327fa8e5bb8285787
Akihito Aiba <akihito1101@gmail.com>
2015-01-04 15:20:52 +0900

    add iAd banner

--- beer73/CameraViewController.swift
+++ beer73/CameraViewController.swift
@@ -9,113 +9,70 @@
 
 import UIKit
 import AVFoundation
+import iAd
 
 class CameraViewController: UIViewController {
     
-    // セッション.
     var mySession : AVCaptureSession!
-    // デバイス.
     var myDevice : AVCaptureDevice!
-    // 画像のアウトプット.
     var myImageOutput : AVCaptureStillImageOutput!
-    
     var myImage : UIImage?
-    var score : Int32 = -1
-
+    
+    var score : Int32 = -1 // -1はエラー
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        // セッションの作成.
         mySession = AVCaptureSession()
         
-        // デバイス一覧の取得.
-        let devices = AVCaptureDevice.devices()
+        let myVideoInput = getVideoInput()
+        mySession.addInput(myVideoInput)
         
+        myImageOutput = AVCaptureStillImageOutput()
+        mySession.addOutput(myImageOutput)
+        
+        // 画像を表示するレイヤーを生成してviewに追加
+        let myVideoLayer = AVCaptureVideoPreviewLayer.layerWithSession(mySession) as AVCaptureVideoPreviewLayer
+        myVideoLayer.frame = self.view.bounds
+        myVideoLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        self.view.layer.addSublayer(myVideoLayer)
+        
+        mySession.startRunning()
+        
+        // 撮影ボタンを生成してviewに追加
+        let myButton = UIButton(frame: CGRectMake(0,0,120,50))
+        myButton.backgroundColor = UIColor.redColor()
+        myButton.layer.masksToBounds = true
+        myButton.setTitle("撮影", forState: .Normal)
+        myButton.layer.cornerRadius = 20.0
+        myButton.layer.position = CGPoint(x: self.view.bounds.width/2, y:self.view.bounds.height-100)
+        myButton.addTarget(self, action: "onClickMyButton:", forControlEvents: .TouchUpInside)
+        self.view.addSubview(myButton)
+        
+        self.canDisplayBannerAds = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        //if mySession.running {
+        //  mySession.stopRunning()
+        //`}
+    }
+    
+    func getVideoInput() -> AVCaptureDeviceInput{
         // バックカメラをmyDeviceに格納.
+        let devices = AVCaptureDevice.devices() // デバイス一覧の取得
         for device in devices{
             if(device.position == AVCaptureDevicePosition.Back){
                 myDevice = device as AVCaptureDevice
             }
         }
-        
         // バックカメラからVideoInputを取得.
-        let videoInput = AVCaptureDeviceInput.deviceInputWithDevice(myDevice, error: nil) as AVCaptureDeviceInput
-        
-        // セッションに追加.
-        mySession.addInput(videoInput)
-        
-        // 出力先を生成.
-        myImageOutput = AVCaptureStillImageOutput()
-        
-        // セッションに追加.
-        mySession.addOutput(myImageOutput)
-        
-        // 画像を表示するレイヤーを生成.
-        let myVideoLayer = AVCaptureVideoPreviewLayer.layerWithSession(mySession) as AVCaptureVideoPreviewLayer
-        myVideoLayer.frame = self.view.bounds
-        myVideoLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
-        
-        // Viewに追加.
-        self.view.layer.addSublayer(myVideoLayer)
-        
-        // セッション開始.
-        mySession.startRunning()
-        
-        // UIボタンを作成.
-        let myButton = UIButton(frame: CGRectMake(0,0,120,50))
-        myButton.backgroundColor = UIColor.redColor();
-        myButton.layer.masksToBounds = true
-        myButton.setTitle("撮影", forState: .Normal)
-        myButton.layer.cornerRadius = 20.0
-        myButton.layer.position = CGPoint(x: self.view.bounds.width/2, y:self.view.bounds.height-50)
-        myButton.addTarget(self, action: "onClickMyButton:", forControlEvents: .TouchUpInside)
-        
-        // UIボタンをViewに追加.
-        self.view.addSubview(myButton);
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-        //if mySession.running {
-          //  mySession.stopRunning()
-        //`}
+        return AVCaptureDeviceInput.deviceInputWithDevice(myDevice, error: nil) as AVCaptureDeviceInput
     }
     
-    //画面遷移
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
-        if (segue.identifier == "shutter") {
-            let resultViewControler: ResultViewController = segue.destinationViewController as ResultViewController
-            resultViewControler.cameraImage = myImage
-            resultViewControler.score = score
-        }
-    }
-    
-    func showAlert() {
-        if let gotModernAlert : AnyClass = NSClassFromString("UIAlertController") {
-            // UIAlertControllerを作成する.
-            let myAlert = UIAlertController(title: "認識エラー", message: "ビールが見えません！", preferredStyle: .Alert)
-            
-            // OKのアクションを作成する.
-            let myOkAction = UIAlertAction(title: "撮り直す", style: .Default) { action in
-                println("Action OK!!")
-            }
-            
-            // OKのActionを追加する.
-            myAlert.addAction(myOkAction)
-            
-            // UIAlertを発動する.
-            self.presentViewController(myAlert, animated: true, completion: nil)
-        } else {
-            let alertView : UIAlertView = UIAlertView(title: "認識エラー", message: "ビールが見えません！", delegate: nil, cancelButtonTitle: "撮り直す")
-            alertView.show()
-        }
-    }
-    
-    // ボタンイベント.
     func onClickMyButton(sender: UIButton){
-        // ビデオ出力に接続.
         let myVideoConnection = myImageOutput.connectionWithMediaType(AVMediaTypeVideo)
         
         // 接続から画像を取得.
@@ -129,10 +86,6 @@ class CameraViewController: UIViewController {
             
             self.score = ScoreCalculator.calcScore(self.myImage)
             
-            // アルバムに追加.
-            //UIImageWriteToSavedPhotosAlbum(myImage, self, nil, nil)
-            //self.score = 100
-            
             if self.score < 0
             {
                 self.showAlert()
@@ -142,6 +95,28 @@ class CameraViewController: UIViewController {
                 self.performSegueWithIdentifier("shutter",sender: nil)
             }
         })
+    }
+    
+    func showAlert() {
+        if let gotModernAlert : AnyClass = NSClassFromString("UIAlertController") {
+            // for iOS8 and later
+            let myAlert = UIAlertController(title: "認識エラー", message: "ビールが見えません！", preferredStyle: .Alert)
+            let myOkAction = UIAlertAction(title: "撮り直す", style: .Default) { action in println("Action OK!!") }
+            myAlert.addAction(myOkAction)
+            self.presentViewController(myAlert, animated: true, completion: nil)
+        } else {
+            let alertView : UIAlertView = UIAlertView(title: "認識エラー", message: "ビールが見えません！", delegate: nil, cancelButtonTitle: "撮り直す")
+            alertView.show()
+        }
+    }
+    
+    //画面遷移
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "shutter") {
+            let resultViewControler: ResultViewController = segue.destinationViewController as ResultViewController
+            resultViewControler.cameraImage = myImage
+            resultViewControler.score = score
+        }
     }
     
     @IBAction func unwindSegueToCamera(segue : UIStoryboardSegue){
